992fe291f1be31d2ea27aef4b25cd1bebaa5c80c
Tycho Bruur <tycho@suppixel.xyz>
2016-07-24 09:22:19 +0200

    Added IslandExit to disable fly.

--- src/xyz/suppixel/SupPixelCore/SkyblockFly.java
+++ src/xyz/suppixel/SupPixelCore/SkyblockFly.java
@@ -7,6 +7,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 import com.wasteofplastic.askyblock.events.IslandEnterEvent;
+import com.wasteofplastic.askyblock.events.IslandExitEvent;
 
 public class SkyblockFly implements Listener {
 	SupPixelCore plugin; // make the variable that is actually just the plugin
@@ -19,15 +20,26 @@ public class SkyblockFly implements Listener {
 	public void onIslandEnter(IslandEnterEvent event) {
 		final UUID owner = (UUID) event.getIslandOwner();
 		final Player ownerusername = plugin.getServer().getPlayer(owner);
-		System.out.println("Island owner UUID: " + owner);
-		System.out.println("Island owner username: " + ownerusername);
 		if (ownerusername.hasPermission("suppixel.islandfly")) {
 			final UUID playerenter = (UUID) event.getPlayer();
 			final Player playerenterusername = plugin.getServer().getPlayer(playerenter);
 			playerenterusername.setAllowFlight(true);
 			playerenterusername.setFlying(true);
 		} else {
+			
+		}
 			// 	
 		}
+		@EventHandler
+		public void onIslandExit(IslandExitEvent event) {
+			final UUID owner = (UUID) event.getIslandOwner();
+			final Player ownerusername = plugin.getServer().getPlayer(owner);
+			if (ownerusername.hasPermission("suppixel.islandfly")) {
+				final UUID playerenter = (UUID) event.getPlayer();
+				final Player playerenterusername = plugin.getServer().getPlayer(playerenter);
+				playerenterusername.setAllowFlight(false);
+				playerenterusername.setFlying(false);
+			} else {
 	}
+}
 }
